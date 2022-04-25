8ae4534aeaf84c8f3ae6f157343a342917210d6d
Rhett Harrison <reharri7@asu.edu>
2022-04-25 16:56:52 -0700

    Login component is in place

---
+++ src/app/pages/login/login.module.ts
@@ -0,0 +1,17 @@
+import {IonicModule} from '@ionic/angular';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {LoginComponent} from './login.component';
+import {LoginRoutingModule} from './login-routing.module';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    LoginRoutingModule
+  ],
+  declarations: [LoginComponent]
+})
+export class LoginComponentModule {}
