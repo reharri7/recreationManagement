9b6f7e74fd53dc07586cc15b94c4ed0b0ab8089b
Rhett Harrison <reharri7@asu.edu>
2022-05-20 07:36:52 -0700

    Refactoring games and teams pages

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -7,7 +7,6 @@ import {IonicModule} from '@ionic/angular';
 import {GamePageRoutingModule} from './game-routing.module';
 
 import {GamePage} from './game.page';
-import {TeamCardComponent} from "../team-card/team-card.component";
 
 @NgModule({
   imports: [
@@ -16,6 +15,6 @@ import {TeamCardComponent} from "../team-card/team-card.component";
     IonicModule,
     GamePageRoutingModule
   ],
-  declarations: [GamePage, TeamCardComponent]
+  declarations: [GamePage]
 })
 export class GamePageModule {}
--- src/app/game/game.page.ts
+++ src/app/game/game.page.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {ToastService} from '../services/toast.service';
 
 @Component({
   selector: 'app-game',
@@ -6,18 +7,27 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./game.page.scss'],
 })
 export class GamePage implements OnInit {
-  public teams = [{
-
-  },
-    {
-
-    },
-    {
-
-    }];
-  constructor() { }
+  public teams = [];
+  public currentNumber = 0;
+  constructor(private presentToastService: ToastService) { }
 
   ngOnInit() {
+  }
+
+  public async incrementTeamScore() {
+    if(this.currentNumber < 100) {
+      this.currentNumber++;
+    } else {
+      await this.presentToastService.presentToast('Team score cannot reach 100', 3000, 'danger');
+    }
+  }
+
+  public async decrementTeamScore() {
+    if(this.currentNumber > 0) {
+      this.currentNumber--;
+    } else {
+      await this.presentToastService.presentToast('Team score cannot be below 0', 3000, 'danger');
+    }
   }
 
 }
